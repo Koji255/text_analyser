aadcf4e54451d0518ff5eb3ea6fc843b682e096d
koji <koji25ony1@gmail.com>
2024-11-01 10:13:57 +0300

    base concept

--- analyser.py
+++ analyser.py
@@ -1,1 +1,11 @@
-print('hello')
+
+"""will be some path in somefile varriable"""
+somefile = "" 
+text = ""
+
+try:
+    with open(somefile) as file:
+        text = [word for word in file]
+
+except:
+    raise ValueError("Something went wrong")
