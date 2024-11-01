77b32081ea2817520e96be348b26c3815f83df79
Koji255 <koji25ony1@gmail.com>
2024-11-01 23:34:09 +0300

    realising comand line argument

--- analyser.py
+++ analyser.py
@@ -1,18 +1,27 @@
+"""
+Tasks List:
+1. Comand Line Arguments
+2. methods.py realisation
+3. methods.py integration & making exceptions
+4. Custom skull fake loading before results 
+"""
+
+import sys
 import methods
 
-"""will be some path in somefile varriable"""
-somefile = "" 
+# sys.exit("No such a file") if (path := sys.argv[1]) else 0
+try:
+    path = sys.argv[1]
+
+except:
+    sys.exit("Please rerun the programm and enter path to the file as first comand line argument: 'python3 analyser.py your_path'")
+
 text = ""
 
 try:
-    with open(somefile) as file:
-        text = [word for word in file]
-
-except ValueError:
-    raise ValueError("Value Error")
-
-else:
-    raise NameError("Something went wrong")
-
-
+    with open(path) as file:
+        for letter in file:
+            text += letter
+except:
+    sys.exit("No such a file or file extension is not '.txt'")
 
--- methods.py
+++ methods.py
@@ -1,17 +1,19 @@
+def main_statistic():
+    """
+    words, letters and sentences
+    """
+
+    pass
+
+def popular_word():
+    """
+    top 10 most popular words
+    """
+
+def words_frequency():
+    pass
+
 if __name__ == "__main__":
-
-    def main_statistic():
-        """
-        words, letters and sentences
-        """
-        pass
-
-    def popular_word():
-        """
-        top 10 most popular words
-        """
-
-    def words_frequency():
-        pass
-
+    
+    
     main()
